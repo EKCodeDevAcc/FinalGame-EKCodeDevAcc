4e6dd2182b8f1cb264e6c5e35ab4b6c3df75abc3
EKCodeDevAcc <edk926@g.harvard.edu>
2019-05-11 14:35:21 -0400

    background, game over state

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -5,10 +5,12 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
 using Random = UnityEngine.Random;
 
+using UnityEngine.SceneManagement;
+
+
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof(CharacterController))]
-    //[RequireComponent(typeof(AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
@@ -25,9 +27,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
         [SerializeField] private float m_StepInterval;
-        //[SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        //[SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        //[SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -41,10 +40,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private float m_StepCycle;
         private float m_NextStep;
         private bool m_Jumping;
-        //private AudioSource m_AudioSource;
 
-        public Text displayingText;
         private bool goalTrigger;
+
+        public Text timerText;
+        public float currentTime = 0f;
+        public float startingTime = 30f;
+
 
         // Use this for initialization
         private void Start()
@@ -57,11 +59,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_StepCycle = 0f;
             m_NextStep = m_StepCycle / 2f;
             m_Jumping = false;
-            //m_AudioSource = GetComponent<AudioSource>();
             m_MouseLook.Init(transform, m_Camera.transform);
 
             goalTrigger = false;
-            displayingText.text = "";
+
+            currentTime = startingTime;
         }
 
         public void SetRotation(Transform other)
@@ -79,16 +81,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         {
             if (other.gameObject.CompareTag("Car"))
             {
-                goalTrigger = true;
-                SetGameOverText();
-            }
-        }
-
-        private void SetGameOverText()
-        {
-            if (goalTrigger == true)
-            {
-                displayingText.text = "Game Over";
+                SceneManager.LoadScene("GameOverScene");
             }
         }
 
@@ -115,18 +108,19 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
+            
+            //timer
+            currentTime -= 1 * Time.deltaTime;
+
+            string minutes = ((int)currentTime / 60).ToString();
+            string seconds = (currentTime % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds; ;
+
+            if (currentTime <= 0)
+            {
+                SceneManager.LoadScene("GameOverScene");
+            }
         }
-
-
-        /*
-        private void PlayLandingSound()
-        {
-            m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
-            m_NextStep = m_StepCycle + .5f;
-        }
-        */
-
 
         private void FixedUpdate()
         {
@@ -148,16 +142,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             if (m_CharacterController.isGrounded)
             {
                 m_MoveDir.y = -m_StickToGroundForce;
-
-                /*
-                if (m_Jump)
-                {
-                    m_MoveDir.y = m_JumpSpeed;
-                    PlayJumpSound();
-                    m_Jump = false;
-                    m_Jumping = true;
-                }
-                */
             }
             else
             {
@@ -170,16 +154,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             m_MouseLook.UpdateCursorLock();
         }
-
-
-        /*
-        private void PlayJumpSound()
-        {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
-        }
-        */
-
 
         private void ProgressStepCycle(float speed)
         {
@@ -195,27 +169,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
 
             m_NextStep = m_StepCycle + m_StepInterval;
-
-            //PlayFootStepAudio();
         }
-
-        /*
-        private void PlayFootStepAudio()
-        {
-            if (!m_CharacterController.isGrounded)
-            {
-                return;
-            }
-            // pick & play a random footstep sound from the array,
-            // excluding sound at index 0
-            int n = Random.Range(1, m_FootstepSounds.Length);
-            m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-            // move picked sound to index 0 so it's not picked next time
-            m_FootstepSounds[n] = m_FootstepSounds[0];
-            m_FootstepSounds[0] = m_AudioSource.clip;
-        }
-        */
 
 
         private void UpdateCameraPosition(float speed)
